4e8bdf72167b6a2e7939610aedd93ca81db29a57
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-02-04 17:59:59 +0600

    login

--- controllers/auth/Registration.js
+++ controllers/auth/Registration.js
@@ -1,35 +1,42 @@
 const ValidateEmail = require("../../helpers/ValidateEmail")
 const validatePassword = require("../../helpers/validatePassword");
 const registrationSchema = require("../../modal/registrationSchema");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const registration = async (req, res)=>{
     const {fullName, email, password} = req.body;
     try {
         if(!fullName){
-            return res.status(400).send("Name is required!")
+            return res.status(400).send({error: "Name is required!"})
         }
         if(!email){
-            return res.status(400).send("Email is required!")
+            return res.status(400).send({error: "Email is required!"})
         }
         if(!ValidateEmail(email)){
-            return res.status(400).send("Email address is invalid!")
+            return res.status(400).send({error: "Email address is invalid!"})
         }
         if(!password){
-            return res.status(400).send("Password is required!")
+            return res.status(400).send({error: "Password is required!"})
         }
         const passwordValidResult = validatePassword(password)
         if(passwordValidResult){
-            return res.status(400).send(passwordValidResult)
+            return res.status(400).send({error: passwordValidResult})
         }
-        
-        const users = await registrationSchema({
-         fullName, email, password
-        })
-    
-        users.save()
-    
-        res.send(users)
+
+        const existingUser = await registrationSchema.findOne({email})
+        if(existingUser){
+            return res.status(400).send({error: "Email already exist! try with another email."}) 
+        }
+
+        bcrypt.hash(password, saltRounds, function(err, hash) {
+            const users = registrationSchema({
+             fullName, email, password: hash
+            })
+            users.save()
+            res.status(200).send({message: "Registration Successfull."})
+        });
     } catch (error) {
-       return res.status(400).send("Server side error! please try again.")
+       return res.status(400).send({error: "Server side error! please try again."})
     }
 }
 
